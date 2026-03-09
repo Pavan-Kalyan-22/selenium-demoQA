9bcaac805d74d6fadab76f8346c030a690971605
Pavan Kalyan <pavankalyanm0103@gmail.com>
2026-03-09 19:20:02 +0530

    removed unused import

--- src/test/java/com/selenium/practice/base/BaseTest.java
+++ src/test/java/com/selenium/practice/base/BaseTest.java
@@ -5,15 +5,10 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import com.selenium.practice.driver.DriverManager;
 import com.selenium.practice.utils.ConfigReader;
 
 public class BaseTest {
