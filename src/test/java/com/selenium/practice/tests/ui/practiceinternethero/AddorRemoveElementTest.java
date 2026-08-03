e6bdd1277d18a04ec4b8606e3c997982d86c69d8
Pavan Kalyan <pavankalyanm0103@gmail.com>
2026-08-03 20:14:27 +0530

    added for loop for the multiple clicks

--- src/test/java/com/selenium/practice/tests/ui/practiceinternethero/AddorRemoveElementTest.java
+++ src/test/java/com/selenium/practice/tests/ui/practiceinternethero/AddorRemoveElementTest.java
@@ -1,7 +1,12 @@
 package com.selenium.practice.tests.ui.practiceinternethero;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import com.selenium.practice.base.AuthenticatedBaseTest;
@@ -13,11 +18,26 @@ public class AddorRemoveElementTest extends BaseTest {
     @Test
     public void AddRemoveElementTest() {
         // Test methods for adding or removing elements will go here
-        driver.navigate().to("https://the-internet.herokuapp.com/add_remove_elements/");
+        driver.navigate().to("https://the-internet.herokuapp.com");
+
+        By label = By.cssSelector("a[href='/add_remove_elements/']");
+
+        WaitUtils.waitForVisibility(driver, label, 10);
+
+        driver.findElement(label).click();
 
         By addElement = By.cssSelector("button[onclick='addElement()']");
         WaitUtils.waitForVisibility(driver, addElement, 10);
-        driver.findElement(addElement).click();
+
+        int click = 0;
+
+        for (int i = 0; i <= 5; i++) {
+            driver.findElement(addElement).click();
+            click++;
+
+        }
+
+        
 
     }
 
